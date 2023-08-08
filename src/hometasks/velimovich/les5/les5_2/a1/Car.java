639c293d8fe4468dd2b0104e8f831c818062d3f3
Maksim_Velimovich <maxim.vel87@gmail.com>
2023-08-08 17:21:11 +0300

    Выполнил задание 5.2 А1

---
+++ src/hometasks/velimovich/les5/les5_2/a1/Car.java
@@ -0,0 +1,4 @@
+package hometasks.velimovich.les5.les5_2.a1;
+
+public class Car {
+}
---
+++ src/hometasks/velimovich/les5/les5_2/a1/Run.java
@@ -0,0 +1,10 @@
+package hometasks.velimovich.les5.les5_2.a1;
+
+public class Run extends Car {
+    public static void main(String[] args){
+        Car mercedes = new Car();
+        Car bmw = new Car();
+        Car audi = new Car();
+    }
+
+}
