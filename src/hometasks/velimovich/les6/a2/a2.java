4506841de48156335733160b30973d3b2e9998e5
Maksim_Velimovich <maxim.vel87@gmail.com>
2023-08-17 15:56:31 +0300

    Выполнил А2

--- src/hometasks/velimovich/les6/a2/a2.java
+++ src/hometasks/velimovich/les6/a2/a2.java
@@ -4,9 +4,11 @@ public class a2 {
 
     public static void main(String[] args) {
         String str = "дом";
+        System.out.println ("Изначальное слово: " + str);
+        System.out.println ("Длина слова: " + str.length());
         String result = str.toUpperCase();
-        System.out.println ("Result: " + result);
-        System.out.print ("Second result: ");
+        System.out.println ("Результат: " + result);
+        System.out.print ("Второй результат: ");
         System.out.println(result.replace('М', 'К'));
     }
 }
