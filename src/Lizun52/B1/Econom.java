bc24808e55457873d60f6f2eb6b6ed992280986e
Nesterovich Alexsander <nesterovich.aleksandr.a@gmail.com>
2023-08-23 15:25:13 +0300

    замечание к заданию 5.2В1

--- src/Lizun52/B1/Econom.java
+++ src/Lizun52/B1/Econom.java
@@ -3,6 +3,6 @@ package Lizun52.B1;
 public class Econom extends Vehicle {
 
     public void move() {
-
+        // а где реализация?) по заданию нужно ещё хоть что-то реализовать в методе.
     }
 }
