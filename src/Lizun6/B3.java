fd6c7ef97299538022e62caef7493582c750640e
Nesterovich Alexsander <nesterovich.aleksandr.a@gmail.com>
2023-08-23 16:09:17 +0300

    замечание к задаче 6В3

--- src/Lizun6/B3.java
+++ src/Lizun6/B3.java
@@ -9,11 +9,14 @@ public class B3 {
         System.out.println("Введите предложение ");
         B3.countVowel();
     }
+
+    // выполнить условие задачи: Написать метод, который на вход принимает строку
     static void countVowel() {
         Scanner sc = new Scanner(System.in);
         String text = sc.nextLine();
 
         Pattern vowel = Pattern.compile("(?iu)[аеёиоуыэюяeyuioa]");
+        // а вот тут хорошо что и английские гласные учтены) я про это даже не подумал)) после устранения замечания этот коммент можно удалить)
 
         Matcher m = vowel.matcher(text);
         int vowelCounter = 0;
@@ -22,4 +25,6 @@ public class B3 {
         }
         System.out.println("В вашем предложении " + vowelCounter + " гласных ");
     }
+    //задача: а на выходе говорит сколько там гласных.
+    //я хотел увидеть метод, который возвратит число гласных. а в main уже вывести это число. как лучше тогда задание переформулировать?
 }
