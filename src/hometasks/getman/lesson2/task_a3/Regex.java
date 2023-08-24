fbdf387aa13ccc69ad4c7f68b4f2a6a8aeaf5a7b
Nesterovich Alexsander <nesterovich.aleksandr.a@gmail.com>
2023-08-24 10:33:30 +0300

    перечитай условие задачи 6А3

--- src/hometasks/getman/lesson2/task_a3/Regex.java
+++ src/hometasks/getman/lesson2/task_a3/Regex.java
@@ -3,7 +3,7 @@ package hometasks.getman.lesson2.task_a3;
 import hometasks.getman.util.Util;
 
 import java.util.regex.Pattern;
-
+// код написан красиво. но я не могу понять, где ты взял условие для этой задачи?))
 public class Regex {
     public final static String REGEX_NUM_PHONE = "((8|\\+7|\\+375)[- ]?)(\\(?\\d{1,3}\\)?[- ]?)?[\\d- ]{7,10}";
     public final static String REGEX_EMAIL = "[A-Za-z0-9+_.-]{3,}+@(\\w+\\.+\\w{1,3})";
