d8679f31ab102a1bd066071151cb70d5a36db933
GetmanYri <noglum@yandex.ru>
2023-08-24 14:04:09 +0300

    Добавил нахождение предлогов в задании 6 В5.

--- src/hometasks/getman/lesson2/task_b5/VershVowel.java
+++ src/hometasks/getman/lesson2/task_b5/VershVowel.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 
 public class VershVowel {
 
-    // предоги тоже считаются словами, т. е. "У"  "і" тоже должно находить. задача решена на половину, подправить и удалить мой коммент
     public static void main(String[] args) {
         Pattern vowel = Pattern.compile("\\s[аеёіоуыэюя][\\u0430-\\u044F\\u0451\\u0401\\u0456\\u045E]*");
         Matcher findVowel = vowel.matcher(Versh.versh.toLowerCase());
