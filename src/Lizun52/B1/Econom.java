99f68dd2669537bf89528a905d941a3edcb53395
Natali3004 <n.lizun3004@gmail.com>
2023-08-27 13:12:02 +0300

    5_2 задача В1 добавила реализацию метода

--- src/Lizun52/B1/Econom.java
+++ src/Lizun52/B1/Econom.java
@@ -3,6 +3,8 @@ package Lizun52.B1;
 public class Econom extends Vehicle {
 
     public void move() {
+        System.out.println("You need to move slowly");
         // а где реализация?) по заданию нужно ещё хоть что-то реализовать в методе.
+
     }
 }
