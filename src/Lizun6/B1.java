99961ec8277fc8f23d6fc03ab46b0fded38e0253
Natali3004 <n.lizun3004@gmail.com>
2023-08-29 14:11:05 +0300

    6 задача В1 добавила параметр

--- src/Lizun6/B1.java
+++ src/Lizun6/B1.java
@@ -1,18 +1,12 @@
 package Lizun6;
 
-import java.util.Scanner;
-
 public class B1 {
     public static void main(String[] args) {
-        System.out.println("Введите слово или число");
-        B1.palindrome();
+
+        B1.palindrome("ioi");
     }
 
-    // Напишите метод, который на вход принимает слово, а на выходе говорит это слово палиндром или нет.
-    // что-то не вижу методов здесь, которые на вход принимают строку)
-    static void palindrome() {
-        Scanner scanner = new Scanner(System.in);
-        String word = scanner.nextLine();
+    public static void palindrome(String word) {
 
         StringBuilder builder = new StringBuilder(word);
         builder.reverse();
