f99c2953bae7771fea1048d432e91ca7fde0f4f7
Natali3004 <n.lizun3004@gmail.com>
2023-08-29 14:36:49 +0300

    6 задача В3 добавила параметр

--- src/Lizun6/B3.java
+++ src/Lizun6/B3.java
@@ -1,30 +1,22 @@
 package Lizun6;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class B3 {
     public static void main(String[] args) {
-        System.out.println("Введите предложение ");
-        B3.countVowel();
+
+        countVowel("I love Java.  And I will learn it. Я люблю Джава...");
+
     }
 
-    // выполнить условие задачи: Написать метод, который на вход принимает строку
-    static void countVowel() {
-        Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
-
+    public static void countVowel(String word) {
         Pattern vowel = Pattern.compile("(?iu)[аеёиоуыэюяeyuioa]");
-        // а вот тут хорошо что и английские гласные учтены) я про это даже не подумал)) после устранения замечания этот коммент можно удалить)
-
-        Matcher m = vowel.matcher(text);
+        Matcher m = vowel.matcher(word);
         int vowelCounter = 0;
         while (m.find()) {
             vowelCounter++;
         }
         System.out.println("В вашем предложении " + vowelCounter + " гласных ");
     }
-    //задача: а на выходе говорит сколько там гласных.
-    //я хотел увидеть метод, который возвратит число гласных. а в main уже вывести это число. как лучше тогда задание переформулировать?
 }
