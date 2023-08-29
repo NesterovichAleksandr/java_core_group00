0139b5d31ddff7a107f1795e5022d8b219dfa4b8
Natali3004 <n.lizun3004@gmail.com>
2023-08-29 14:15:27 +0300

    6 задача В2 добавила параметр

--- src/Lizun6/B2.java
+++ src/Lizun6/B2.java
@@ -1,17 +1,13 @@
 package Lizun6;
 
-import java.util.Scanner;
-
 public class B2 {
     public static void main(String[] args) {
-        System.out.println("Введите предложение ");
-        B2.countWord();
+
+        B2.countWord("I love Java.  And I will learn     it.");
     }
 
     // тут аналогично с предыдущим заданием. нет метода, который принимает на вход строку.
-    static void countWord() {
-        Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
+    static void countWord(String text) {
 
         String[] words = text.split("\\s*(\\s|,|!|\\.)\\s*");
         System.out.print("В вашем предложении " + words.length + " слов.");
