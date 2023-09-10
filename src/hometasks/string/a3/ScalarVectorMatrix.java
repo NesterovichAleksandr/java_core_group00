405052fcac247048c78d662825f579a0217d9021
Anna <madam.sestra@gmail.com>
2023-09-10 21:59:16 +0300

    string topic

--- src/hometasks/string/a3/ScalarVectorMatrix.java
+++ src/hometasks/string/a3/ScalarVectorMatrix.java
@@ -1,2 +1,17 @@
-package hometasks.string.a3;public class ScalarVectorMatrix {
+package hometasks.string.a3;
+
+public class ScalarVectorMatrix {
+    public static void main(String[] args) {
+
+    }
+
+    public static void scalarVectorMatrix(String string) {
+
+
+    }
+
 }
+/*
+Написать метод, который на вход принимает строку, а на выводе
+говорит, это скаляр, вектор или матрица.
+ */
