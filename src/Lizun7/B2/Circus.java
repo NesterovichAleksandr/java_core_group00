7de2fa125078791f889e2609ccffc771a64d4e63
Natali3004 <n.lizun3004@gmail.com>
2023-09-24 14:45:21 +0300

    В2 из 7 темы

---
+++ src/Lizun7/B2/Circus.java
@@ -0,0 +1,26 @@
+package Lizun7.B2;
+
+public class Circus {
+    String name;
+    Bear bear;
+    Elephant elephant;
+    Horse horse;
+
+    public static class Bear {
+        void iCan() {
+            System.out.println("Я умею ходить");
+        }
+    }
+
+    public static class Elephant {
+        void iCan() {
+            System.out.println("Я умею ходить");
+        }
+    }
+
+    public static class Horse {
+        void iCan() {
+            System.out.println("Я умею ходить");
+        }
+    }
+}
