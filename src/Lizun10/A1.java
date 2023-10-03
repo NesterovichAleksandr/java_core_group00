3046ff503251fecdf46492398241bd3df4742dec
Natali3004 <n.lizun3004@gmail.com>
2023-10-03 13:50:53 +0300

    A1 И А2 из 10 темы

---
+++ src/Lizun10/A1.java
@@ -0,0 +1,21 @@
+package Lizun10;
+
+import java.util.ArrayList;
+
+public class A1 {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> rainbow = new ArrayList<>();
+        rainbow.add("Red");
+        rainbow.add("Orange");
+        rainbow.add("Yellow");
+        rainbow.add("Green");
+        rainbow.add("Blue");
+        rainbow.add("Dark Blue");
+        rainbow.add("Purple");
+
+        System.out.println(rainbow);
+
+    }
+}
