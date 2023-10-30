65e28c54314b8cd56ffaf0a17b37e59ed5bdc48a
Natali3004 <n.lizun3004@gmail.com>
2023-10-30 12:21:56 +0300

    доделала В1,В2,B3,B4 из 9 темы.

--- src/Lizun9/В1/Run.java
+++ src/Lizun9/В1/Run.java
@@ -3,11 +3,10 @@ package Lizun9.В1;
 public class Run {
     public static void main(String[] args) {
         Array myMassive = new Array();
-
         myMassive.setElement(78);
-        myMassive.setElement(8);
+        myMassive.setElement("5");
         myMassive.setElement(6);
-        myMassive.setElement(10);
+        myMassive.setElement("125");
         myMassive.setElement(25);
         myMassive.setElement(453);
         myMassive.setElement(1);
@@ -16,17 +15,17 @@ public class Run {
         myMassive.setElement(259);
         myMassive.setElement(111111);
 
-
         myMassive.printArray();
 
         myMassive.getElement(2);
 
-        myMassive.contains(255822);
+        myMassive.contains(259);
 
-        myMassive.getIndex(8);
+        myMassive.getIndex(1058);
 
         myMassive.delete(2);
 
-        myMassive.delete("10");
+        Integer man = 49;
+        myMassive.delete(man);
     }
 }
