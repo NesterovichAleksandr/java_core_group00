a1a121a9e843a57038ebb038d1b7a7662b96c0d7
Parshin Evgeny <Gravity272@gmail.com>
2023-11-02 18:00:17 +0300

    Lesson 7 level A task 2 - fully done

--- src/hometasks/lesson7/lvlA/task1/Run.java
+++ src/hometasks/lesson7/lvlA/task1/Run.java
@@ -4,7 +4,6 @@ package hometasks.lesson7.lvlA.task1;
   Внутренний класс Student должен хранить информацию о студенте.
   Создайте класс Run и в методе main выведите в консоль информацию о студенте.*/
 
-/* */
 public class Run {
     public static void main(String[] args) {
         University.Student Ivanov = new University().new Student("Ivan", 23);
