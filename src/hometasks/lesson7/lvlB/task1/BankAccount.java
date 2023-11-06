b82d6aa4525fb66add92f40ea9351c66d4f74128
Parshin Evgeny <Gravity272@gmail.com>
2023-11-06 15:12:02 +0300

    Lesson 7 level B task 1 - Fully done

---
+++ src/hometasks/lesson7/lvlB/task1/BankAccount.java
@@ -0,0 +1,48 @@
+package hometasks.lesson7.lvlB.task1;
+
+import java.math.BigDecimal;
+
+public class BankAccount {
+    private long accountId;
+    private BigDecimal balance = BigDecimal.valueOf(0.0);
+
+    BankAccount() {
+        accountId = accountIdGen.nextAccountId;
+        accountIdGen.generateNextAccountId();
+    }
+
+    Transaction transaction = new Transaction();
+    AccountIdGen accountIdGen = new AccountIdGen();
+
+    private static class AccountIdGen {
+        private long nextAccountId = 1;
+
+        private void generateNextAccountId() {
+            nextAccountId++;
+        }
+    }
+
+    class Transaction {
+        //Перевести деньги на другой счёт
+        public void transferTo(BankAccount receiver, BigDecimal moneyAmount) {
+            receiver.transaction.topUp(moneyAmount);
+            balance = balance.add(moneyAmount.negate());
+        }
+
+        //Пополнить текущий счёт на сумму
+        public void topUp(BigDecimal moneyAmount) {
+            balance = balance.add(moneyAmount);
+        }
+
+        //Информация о текущем счёте
+        public void accountInfo() {
+            System.out.println("Owner account ID: " + accountId + '\n'
+                    + "Account balance: " + balance);
+        }
+
+        //Снять со счёта сумму
+        public void withdrawFromAccount(BigDecimal moneyAmount) {
+            balance = balance.add(moneyAmount.negate());
+        }
+    }
+}
---
+++ src/hometasks/lesson7/lvlB/task1/Run.java
@@ -0,0 +1,29 @@
+package hometasks.lesson7.lvlB.task1;
+
+import java.math.BigDecimal;
+
+/*Создать класс BankAccount, который представляет банковский счет. Каждый банковский счет имеет
+уникальный номер и баланс. Класс BankAccount должен содержать внутренний класс Transaction,
+который представляет транзакцию на счете. Внутренний класс Transaction должен иметь методы для
+пополнения счёта и снятия средств с баланса, а также для получения информации о текущем балансе.*/
+
+public class Run {
+    public static void main(String[] args) {
+        BankAccount ivanov = new BankAccount();
+        BankAccount petrov = new BankAccount();
+
+
+        ivanov.transaction.topUp(BigDecimal.valueOf(200.05));
+        ivanov.transaction.accountInfo();
+
+        petrov.transaction.accountInfo();
+
+        ivanov.transaction.transferTo(petrov, BigDecimal.valueOf(103.87));
+
+        petrov.transaction.accountInfo();
+        ivanov.transaction.accountInfo();
+
+        ivanov.transaction.withdrawFromAccount(BigDecimal.valueOf(50));
+        ivanov.transaction.accountInfo();
+    }
+}
